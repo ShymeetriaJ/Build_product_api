949bc5dbae1f8c6cab81ef22b5fa9a17070db739
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-20 00:32:04 -0500

    tested all routes for success.

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ require('./config/connection');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
