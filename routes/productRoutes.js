6a528439675213b07443d2478d54d785863081be
ShymeetriaJ <sjeducates2@gmail.com>
2026-01-19 22:33:07 -0500

    completed product routes file.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,3 +11,98 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true, runValidators: true }
+        );
+        
+        if (!updatedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+        
+        if (!deletedProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        
+        res.status(200).json({ message: 'Product deleted successfully', product: deletedProduct });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const { category, minPrice, maxPrice, sortBy, page = 1, limit = 10 } = req.query;
+
+        const filter = {};
+        if (category) {
+            filter.category = category;
+        }
+
+        if (minPrice || maxPrice) {
+            filter.price = {};
+            if (minPrice) {
+                filter.price.$gte = Number(minPrice);
+            }
+            if (maxPrice) {
+                filter.price.$lte = Number(maxPrice);
+            }
+        }
+        
+        let sort = {};
+
+        if (sortBy) {
+            if (sortBy === 'price_asc') {
+                sort.price = 1;
+            } else if (sortBy === 'price_desc') {
+                sort.price = -1;
+            } else if (sortBy === 'name_asc') {
+                sort.name = 1;
+            }   else if (sortBy === 'name_desc') {
+                sort.name = -1;
+            }
+        }
+
+        const pageNum = parseInt(page);
+        const limitNum = parseInt(limit);
+        const skip = (pageNum - 1) * limitNum;
+
+        const products = await Product.find(filter)
+        .sort(sort)
+        .skip(skip)
+        .limit(limitNum);
+
+        res.status(200).json(products);
+    
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
